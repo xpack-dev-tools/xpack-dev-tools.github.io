d400a47f20a3df1a075d85d12826f70d53a9ef1c
Liviu Ionescu <ilg@livius.net>
2024-12-11 12:35:19 +0200

    website sunrise-og-image update

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -261,8 +261,9 @@ const config: Config = {
   ],
   // https://docusaurus.io/docs/seo
   themeConfig: {
-    // Replace with your project's social card
-    // image: 'img/docusaurus-social-card.jpg',
+    // The project's social card, og:image, twitter:image, 1200x630
+    image: 'img/sunrise-og-image.jpg',
+
     metadata: [
       {
         name: 'keywords',
