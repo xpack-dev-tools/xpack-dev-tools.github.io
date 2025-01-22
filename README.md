328488b46e804ed257fe68bf6ad2bfbcc872db35
Liviu Ionescu <ilg@livius.net>
2025-01-23 00:33:17 +0200

    website: re-generate commons

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
-# The xPack Binary Development Tools Web Site
 
-This project provides the content of the
-[`xpack-dev-tools`](https://github.com/xpack-dev-tools) GitHub organization.
+[![license](https://img.shields.io/github/license/xpack-dev-tools/xpack-dev-tools.github.io)](https://github.com/xpack-dev-tools/xpack-dev-tools.github.io/blob/website/LICENSE)
 
-The actual content is in the `website` folder, and is automatically published
-on GitHub Pages from the `website` branch.
+# The xPack Binary Development Tools web site source
+
+The Docusaurus source code for the xPack Binary Development Tools web site.
+
+## Project source
+
+The source code is available on
+[GitHub](https://github.com/xpack-dev-tools/xpack-dev-tools.github.io/).
+
+## License
+
+Unless otherwise stated, the original content is released under the terms of the
+[MIT License](https://opensource.org/licenses/mit/),
+with all rights reserved to
+[Liviu Ionescu](https://github.com/ilg-ul).
--- website/src/components/HomepageTools/index.tsx
+++ website/src/components/HomepageTools/index.tsx
@@ -30,7 +30,7 @@ function Tool({ longName, shortName }) {
         <div className="padding-top--xs">
           <Link to={'https://github.com/xpack-dev-tools/' + shortName + '-xpack/releases/'}><img alt="GitHub Release" src={'https://img.shields.io/github/v/release/xpack-dev-tools/' + shortName + '-xpack?color=blue'} /></Link>
           &nbsp;<Link to={'https://github.com/xpack-dev-tools/' + shortName + '-xpack/releases/'}><img alt="GitHub Release Date" src={'https://img.shields.io/github/release-date/xpack-dev-tools/' + shortName + '-xpack?label=date&color=yellowgreen'} /></Link>
-          &nbsp;<Link to={'https://github.com/xpack-dev-tools/' + shortName + '-xpack/releases/'}><img alt="GitHub Downloads (all assets, all releases)" src={'https://img.shields.io/github/downloads/xpack-dev-tools/' + shortName + '-xpack/total.svg'} /></Link>
+          &nbsp;<Link to={'https://github.com/xpack-dev-tools/' + shortName + '-xpack/releases/'}><img alt="GitHub Downloads (all assets, all releases)" src={'https://img.shields.io/github/downloads/xpack-dev-tools/' + shortName + '-xpack/total'} /></Link>
           &nbsp;<Link to={'https://github.com/xpack-dev-tools/' + shortName + '-xpack/'}><img alt="GitHub Repo stars" src={'https://img.shields.io/github/stars/xpack-dev-tools/' + shortName + '-xpack'} /></Link>
         </div>
       </div>
